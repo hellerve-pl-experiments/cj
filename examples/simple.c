8ded42ceb33419be6cc130ad10ab35e8fee2400b
Veit Heller <veit@veitheller.de>
2025-10-19 22:55:45 -0500

    unify simple example and extend readme a bit

--- examples/simple.c
+++ examples/simple.c
@@ -1,18 +1,12 @@
 #include "ctx.h"
 #include "op.h"
 
+// this one works on both backends
 int main() {
   cj_ctx* cj = create_cj_ctx();
 
-#ifdef __aarch64__
-  // ARM64: NOP and RET
   cj_nop(cj);
   cj_ret(cj);
-#else
-  // x86-64: NOP and RET
-  cj_nop(cj);
-  cj_ret(cj);
-#endif
 
   cj_fn f = create_cj_fn(cj);
 
